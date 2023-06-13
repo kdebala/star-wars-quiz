b96a92b8d9f91584420038113ee1a023d2da4af1
Katarzyna Dębała <kdebala@wp.pl>
2023-06-13 09:39:50 +0200

    add timer

--- src/components/timerPanel/index.ts
+++ src/components/timerPanel/index.ts
@@ -1,15 +1,17 @@
+import '../timerPanel/_timer-panel.scss';
 import createElement from '../../utils/createElement';
+import LightSaberSrc from '../../assets/others/LightsaberHandle.png';
 const createTimerPanel = () => {
   const sectionEl = createElement('section', 'timer-panel');
-  const LightsaberArea = createElement('div', 'lightsaber-area');
-  const lightsaberImg = createElement(
-    'img',
-    'lightsaberImg',
-    '',
-    'lightsaber-img',
+  const saberLight = createElement('div', 'timer-panel__lightsaber');
+  const LightsaberArea = createElement(
+    'div',
+    'timer-panel__lightsaber-area',
+    LightSaberSrc,
+    'timer-panel__lightsaber-img',
   );
-  const textTimerArea = createElement('div', 'text-timer');
-  LightsaberArea.appendChild(lightsaberImg);
+  const textTimerArea = createElement('div', 'timer-panel__text-timer');
+  LightsaberArea.appendChild(saberLight);
   sectionEl.appendChild(LightsaberArea);
   sectionEl.appendChild(textTimerArea);
   return sectionEl;
